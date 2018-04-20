15fcedcbfb524a48f0d31b2fc2ee7c4976ce84cc
huangwubei <huangwubeihwb@hfbank.com.cn>
2018-04-20 16:31:51 +0800

    upload project

---
+++ src/main/java/com/jenkins/Demo.java
@@ -0,0 +1,10 @@
+package com.jenkins;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a maven project demo using for upload to github, and which can be created by jenkins blueocean from github. ");
+	}
+
+}
---
+++ target/classes/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h3>This is a maven project demo using for upload to github, and which can be created by jenkins blueocean from github. </h3>
+</body>
+</html>
